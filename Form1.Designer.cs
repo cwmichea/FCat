ec0349402cd2b10284795cc7d2b2bbe2100e6df7
cwmichea <cwmichea@uc.cl>
2022-01-31 19:56:54 -0500

    whole project

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace FCat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.scoreText = new System.Windows.Forms.Label();
+            this.labelPause = new System.Windows.Forms.Label();
+            this.bunny = new System.Windows.Forms.PictureBox();
+            this.catFood = new System.Windows.Forms.PictureBox();
+            this.petitC0 = new System.Windows.Forms.PictureBox();
+            this.dog = new System.Windows.Forms.PictureBox();
+            this.petitC3 = new System.Windows.Forms.PictureBox();
+            this.mountain = new System.Windows.Forms.PictureBox();
+            this.petitC4 = new System.Windows.Forms.PictureBox();
+            this.petitC5 = new System.Windows.Forms.PictureBox();
+            this.petitC1 = new System.Windows.Forms.PictureBox();
+            this.petitC2 = new System.Windows.Forms.PictureBox();
+            this.Cloud2 = new System.Windows.Forms.PictureBox();
+            this.Cloud3 = new System.Windows.Forms.PictureBox();
+            this.Cloud1 = new System.Windows.Forms.PictureBox();
+            this.cat = new System.Windows.Forms.PictureBox();
+            this.hit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bunny)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.catFood)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mountain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Lucida Handwriting", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(681, 9);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(100, 31);
+            this.scoreText.TabIndex = 7;
+            this.scoreText.Text = "Score: ";
+            // 
+            // labelPause
+            // 
+            this.labelPause.AutoSize = true;
+            this.labelPause.Font = new System.Drawing.Font("Lucida Handwriting", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPause.Location = new System.Drawing.Point(457, 313);
+            this.labelPause.Name = "labelPause";
+            this.labelPause.Size = new System.Drawing.Size(111, 31);
+            this.labelPause.TabIndex = 14;
+            this.labelPause.Text = "Paused";
+            // 
+            // bunny
+            // 
+            this.bunny.BackColor = System.Drawing.Color.Transparent;
+            this.bunny.Image = global::FCat.Properties.Resources.bunny1;
+            this.bunny.Location = new System.Drawing.Point(1046, 615);
+            this.bunny.Name = "bunny";
+            this.bunny.Size = new System.Drawing.Size(50, 49);
+            this.bunny.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bunny.TabIndex = 16;
+            this.bunny.TabStop = false;
+            // 
+            // catFood
+            // 
+            this.catFood.Image = global::FCat.Properties.Resources.catfoodpsd;
+            this.catFood.Location = new System.Drawing.Point(542, 364);
+            this.catFood.Name = "catFood";
+            this.catFood.Size = new System.Drawing.Size(74, 81);
+            this.catFood.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.catFood.TabIndex = 15;
+            this.catFood.TabStop = false;
+            // 
+            // petitC0
+            // 
+            this.petitC0.BackColor = System.Drawing.Color.Transparent;
+            this.petitC0.Image = global::FCat.Properties.Resources.Cloud4;
+            this.petitC0.Location = new System.Drawing.Point(463, 167);
+            this.petitC0.Name = "petitC0";
+            this.petitC0.Size = new System.Drawing.Size(50, 49);
+            this.petitC0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.petitC0.TabIndex = 12;
+            this.petitC0.TabStop = false;
+            // 
+            // dog
+            // 
+            this.dog.BackColor = System.Drawing.Color.Transparent;
+            this.dog.Image = global::FCat.Properties.Resources.FlyingDog;
+            this.dog.Location = new System.Drawing.Point(1072, 69);
+            this.dog.Name = "dog";
+            this.dog.Size = new System.Drawing.Size(171, 130);
+            this.dog.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dog.TabIndex = 11;
+            this.dog.TabStop = false;
+            // 
+            // petitC3
+            // 
+            this.petitC3.BackColor = System.Drawing.Color.Transparent;
+            this.petitC3.Image = global::FCat.Properties.Resources.Cloud4;
+            this.petitC3.Location = new System.Drawing.Point(962, 579);
+            this.petitC3.Name = "petitC3";
+            this.petitC3.Size = new System.Drawing.Size(50, 49);
+            this.petitC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.petitC3.TabIndex = 6;
+            this.petitC3.TabStop = false;
+            // 
+            // mountain
+            // 
+            this.mountain.BackColor = System.Drawing.Color.Transparent;
+            this.mountain.Image = global::FCat.Properties.Resources.mountain1;
+            this.mountain.Location = new System.Drawing.Point(805, 602);
+            this.mountain.Name = "mountain";
+            this.mountain.Size = new System.Drawing.Size(207, 62);
+            this.mountain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mountain.TabIndex = 10;
+            this.mountain.TabStop = false;
+            // 
+            // petitC4
+            // 
+            this.petitC4.BackColor = System.Drawing.Color.Transparent;
+            this.petitC4.Image = global::FCat.Properties.Resources.Cloud4;
+            this.petitC4.Location = new System.Drawing.Point(1072, 278);
+            this.petitC4.Name = "petitC4";
+            this.petitC4.Size = new System.Drawing.Size(99, 66);
+            this.petitC4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.petitC4.TabIndex = 9;
+            this.petitC4.TabStop = false;
+            // 
+            // petitC5
+            // 
+            this.petitC5.BackColor = System.Drawing.Color.Transparent;
+            this.petitC5.Image = global::FCat.Properties.Resources.Cloud4;
+            this.petitC5.Location = new System.Drawing.Point(395, 222);
+            this.petitC5.Name = "petitC5";
+            this.petitC5.Size = new System.Drawing.Size(95, 55);
+            this.petitC5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.petitC5.TabIndex = 8;
+            this.petitC5.TabStop = false;
+            // 
+            // petitC1
+            // 
+            this.petitC1.BackColor = System.Drawing.Color.Transparent;
+            this.petitC1.Image = global::FCat.Properties.Resources.Cloud4;
+            this.petitC1.Location = new System.Drawing.Point(291, 93);
+            this.petitC1.Name = "petitC1";
+            this.petitC1.Size = new System.Drawing.Size(77, 55);
+            this.petitC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.petitC1.TabIndex = 5;
+            this.petitC1.TabStop = false;
+            // 
+            // petitC2
+            // 
+            this.petitC2.BackColor = System.Drawing.Color.Transparent;
+            this.petitC2.Image = global::FCat.Properties.Resources.Cloud4;
+            this.petitC2.Location = new System.Drawing.Point(585, 161);
+            this.petitC2.Name = "petitC2";
+            this.petitC2.Size = new System.Drawing.Size(90, 55);
+            this.petitC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.petitC2.TabIndex = 4;
+            this.petitC2.TabStop = false;
+            // 
+            // Cloud2
+            // 
+            this.Cloud2.BackColor = System.Drawing.Color.Transparent;
+            this.Cloud2.Image = global::FCat.Properties.Resources.Cloud3;
+            this.Cloud2.Location = new System.Drawing.Point(463, 69);
+            this.Cloud2.Name = "Cloud2";
+            this.Cloud2.Size = new System.Drawing.Size(212, 147);
+            this.Cloud2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cloud2.TabIndex = 3;
+            this.Cloud2.TabStop = false;
+            // 
+            // Cloud3
+            // 
+            this.Cloud3.BackColor = System.Drawing.Color.Transparent;
+            this.Cloud3.Image = global::FCat.Properties.Resources.Cloud2;
+            this.Cloud3.Location = new System.Drawing.Point(687, 525);
+            this.Cloud3.Name = "Cloud3";
+            this.Cloud3.Size = new System.Drawing.Size(166, 139);
+            this.Cloud3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cloud3.TabIndex = 2;
+            this.Cloud3.TabStop = false;
+            // 
+            // Cloud1
+            // 
+            this.Cloud1.BackColor = System.Drawing.Color.Transparent;
+            this.Cloud1.Image = global::FCat.Properties.Resources.Cloud1;
+            this.Cloud1.Location = new System.Drawing.Point(291, 1);
+            this.Cloud1.Name = "Cloud1";
+            this.Cloud1.Size = new System.Drawing.Size(210, 147);
+            this.Cloud1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cloud1.TabIndex = 1;
+            this.Cloud1.TabStop = false;
+            // 
+            // cat
+            // 
+            this.cat.AccessibleName = "cat";
+            this.cat.Image = global::FCat.Properties.Resources.FlyingCat;
+            this.cat.Location = new System.Drawing.Point(12, 245);
+            this.cat.Name = "cat";
+            this.cat.Size = new System.Drawing.Size(113, 89);
+            this.cat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cat.TabIndex = 0;
+            this.cat.TabStop = false;
+            // 
+            // hit
+            // 
+            this.hit.Image = ((System.Drawing.Image)(resources.GetObject("hit.Image")));
+            this.hit.Location = new System.Drawing.Point(3, 245);
+            this.hit.Name = "hit";
+            this.hit.Size = new System.Drawing.Size(146, 116);
+            this.hit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.hit.TabIndex = 13;
+            this.hit.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1093, 665);
+            this.Controls.Add(this.bunny);
+            this.Controls.Add(this.catFood);
+            this.Controls.Add(this.labelPause);
+            this.Controls.Add(this.hit);
+            this.Controls.Add(this.petitC0);
+            this.Controls.Add(this.dog);
+            this.Controls.Add(this.petitC3);
+            this.Controls.Add(this.mountain);
+            this.Controls.Add(this.petitC4);
+            this.Controls.Add(this.petitC5);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.petitC1);
+            this.Controls.Add(this.petitC2);
+            this.Controls.Add(this.Cloud2);
+            this.Controls.Add(this.Cloud3);
+            this.Controls.Add(this.Cloud1);
+            this.Controls.Add(this.cat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KUp);
+            ((System.ComponentModel.ISupportInitialize)(this.bunny)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.catFood)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mountain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.petitC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cat;
+        private System.Windows.Forms.PictureBox Cloud1;
+        private System.Windows.Forms.PictureBox Cloud3;
+        private System.Windows.Forms.PictureBox Cloud2;
+        private System.Windows.Forms.PictureBox petitC2;
+        private System.Windows.Forms.PictureBox petitC1;
+        private System.Windows.Forms.PictureBox petitC3;
+        private System.Windows.Forms.Label scoreText;
+        public System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.PictureBox petitC5;
+        private System.Windows.Forms.PictureBox petitC4;
+        private System.Windows.Forms.PictureBox mountain;
+        private System.Windows.Forms.PictureBox dog;
+        private System.Windows.Forms.PictureBox petitC0;
+        private System.Windows.Forms.Label labelPause;
+        private System.Windows.Forms.PictureBox catFood;
+        private System.Windows.Forms.PictureBox bunny;
+        private System.Windows.Forms.PictureBox hit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;//Media libray for SoundPlayer 
+
+namespace FCat
+{
+    public partial class Form1 : Form
+    {   //Create window form file, make difference between regular and customized toolbox elements
+        //Add, place, name, choose and get the picture boxes
+        //Add and enable Timer and time event, start coding in GameTimerEv.
+        //Enable Key down and up events with spacebar and P
+        //Create variables included SoundPlayer that needs a Media libray
+        int speed = 8;
+        int gravityForce = 8;
+        int score = 0;
+        int previousScore = 0;
+        int level = 0;
+        int catF = 0;
+        bool gameOn = true;
+        public SoundPlayer soundPlay;
+        public SoundPlayer getFoodPlay;
+        public SoundPlayer crashPlay;
+        bool playingASound = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            soundPlay = new SoundPlayer("cat1.wav");
+            getFoodPlay = new SoundPlayer("hungrycat.wav");
+            crashPlay = new SoundPlayer("kittycrash.wav");
+        }
+
+        private void KDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) { gravityForce = -8; }//Set to negative the gravity force. Negative value is UP
+            if (e.KeyCode == Keys.P) { labelPause.Visible = true; gameOn = !gameOn; }//Set on pause the game
+        }
+
+        private void KUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) 
+            { gravityForce = 8; 
+                if(playingASound==false) { soundPlay.Play(); } 
+            }//Switch back to normal the gravity force. Positive value is DOWN. Also play sound only if there is no other sound played
+        }
+
+        private void GameTimer(object sender, EventArgs e)
+        {
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (gameOn)//Game is on only when is not paused or it is game over
+            {
+                //Score text updating
+                scoreText.Text = "Score: " + score.ToString() + " level " + level.ToString() + " CatFood " + catF.ToString(); //+ " level " + speed.ToString() + " prescore " + previousScore.ToString() + " prescore " + speed.ToString();   //Show and update score
+                //Basic Gameplay 
+                labelPause.Visible = false;             //Hide paused text because Game is on
+                level = speed - 8;                      //Get a level parameter connected to the speed of the game
+                cat.Top += gravityForce;                //Player goes UP
+                hit.Top = cat.Top;                      //Hit effect follows cat position
+                hit.Visible = false;                    //Hit is invisible during the game, only appears when it crashes
+                //Make things move left
+                petitC0.Left -= speed;                  //Make the little clouds go left
+                petitC1.Left -= speed;
+                petitC2.Left -= speed;
+                petitC3.Left -= speed;
+                petitC4.Left -= speed + 2;
+                petitC5.Left -= speed + 1;
+
+                catFood.Left -= speed;                  //Make the catfood goes left
+
+                mountain.Left -= speed;                 //Make the mountain goes left
+                dog.Left -= speed;                      //Make the dog goes left and down
+                dog.Top += (speed / 3);
+
+                Cloud1.Left -= speed;                   //Make the big clouds go left
+                Cloud2.Left -= speed;
+                Cloud3.Left -= speed;
+
+                if (petitC0.Left < -55)                 //Make the small clouds teleport to the right, out the window, to cylce it
+                { petitC0.Left = 1111; score++; }
+                if (petitC1.Left < -55)
+                { petitC1.Left = 1111; score++; }
+                if (petitC2.Left < -55)
+                { petitC2.Left = 1111; score++; }
+                if (petitC3.Left < -55)
+                { petitC3.Left = 1111; score++; }
+                if (petitC4.Left < -55)
+                { petitC4.Left = 4444; score++; }
+                if (petitC5.Left < -55)
+                { petitC5.Left = 4444; score++; }
+                //Make things reappear
+                if (catFood.Left < -55)                 //Make the catfood teleports to the right, out the window, to cylce it
+                { catFood.Left = 4444; ; }              //For memory performance only, Make cat food keeps moving in the same space, although not visible
+
+                if (Cloud1.Left < -55)                  //Make the big clouds teleport to the right, out the window, to cylce it 
+                { Cloud1.Left = 2222; score++; }
+                if (Cloud2.Left < -55)
+                { Cloud2.Left = 2222; score++; }
+                if (Cloud3.Left < -55)
+                { Cloud3.Left = 2222; score++; }
+
+                if (mountain.Left < -55)               //Make the mountain teleports to the right
+                { mountain.Left = 4444;  ; }
+
+                if (score % 20 == 0 && catFood.Visible == false)  //Make pop up a cat food for every +20 more in score
+                { catFood.Visible = true; catFood.Left = 1111; }
+
+                if (score % 25 == 0 && score != 0)  //Make pop up a dog for every +25 more in score
+                { dog.Left = 1111; dog.Top = 0; }
+
+                //If cat hits sth...
+                if (cat.Bounds.IntersectsWith(catFood.Bounds) && catFood.Visible) //Make the cat food add a catfood point, also block playing another sound
+                { catF++; catFood.Visible = false; playingASound = true;  getFoodPlay.Play(); }
+
+                if (cat.Bounds.IntersectsWith(petitC1.Bounds) || cat.Bounds.IntersectsWith(petitC2.Bounds) || cat.Bounds.IntersectsWith(petitC3.Bounds) ||
+                    cat.Bounds.IntersectsWith(Cloud1.Bounds) || cat.Bounds.IntersectsWith(Cloud2.Bounds) || cat.Bounds.IntersectsWith(Cloud3.Bounds) ||
+                    cat.Bounds.IntersectsWith(mountain.Bounds) || cat.Bounds.IntersectsWith(dog.Bounds) || cat.Bounds.IntersectsWith(petitC0.Bounds) ||
+                    cat.Bounds.IntersectsWith(petitC4.Bounds) || cat.Bounds.IntersectsWith(petitC5.Bounds))
+                { GameOver(); }                         //If cat hits the clouds, mounatin, gameover
+
+                if (cat.Top < -1)                       //If cat goes above the upper border, gameover
+                { GameOver(); }
+                if (cat.Top > 470)                      //If cat goes below the lower border, gameover
+                { GameOver(); }
+
+                //Increase difficulty according to the score range
+                if (score % 10 == 0 && previousScore != score)                         //Increase difficulty linealy +1 for each +10 score
+                {
+                    previousScore = score;
+                    //Make sure  to add Difficulty only when 5 score are added, and not for each time iteration
+                    speed++;
+                }
+                else if (score > 150 && score % 5 == 0 && previousScore != score)       //Increase difficulty linealy +1 for each +5 score
+                {
+                    previousScore = score;
+                    //Make sure  to add Difficulty only when 5 score are added, and not for each time iteration
+                    speed++;
+                }   
+            }
+            else { }//Do nothing if the game is paused
+        }
+        private void GameOver()
+        {
+            playingASound = true;
+            crashPlay.Play();
+            gameOn = false;
+            hit.Visible = true;
+            gametimer.Stop();
+            scoreText.Text += " \nGame over. Good try!";
+        }
+    }
+}
